96ed47d2b979d181452e5bf36709c4131447ce7b
unknown <cristianpipe0494@gmail.com>
2019-10-10 10:25:42 -0600

    add: buttons collection styles

--- src/components/ComponentButtons/buttons.js
+++ src/components/ComponentButtons/buttons.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import './buttons.scss';
+
 const Buttons = {
-    SubscribeBtn: () => <button>Subscribirse</button>,
-    WhiteComunicateBtn: () => <button>Crea un comunicado</button>,
-    PinkComunicateBtn: () => <button>Crea un comunicado</button>,
-    BigComunicateBtn: () => <button>Crea tu comunicado</button>,
-    BasicPackBtn: () => <button>Obtener Básico</button>,
-    StandarPackBtn: () => <button>Obtener Estandar</button>,
-    PremiumPackBtn: () => <button>Obtener Premium</button>,
+    SubscribeBtn: () => <button className= {'layout__btn--standar layout__btn--subscribe'}>Subscribirse</button>,
+    WhiteComunicateBtn: () => <button className= {'layout__btn--standar layout__btn--comunicate-white'}>Crea un comunicado</button>,
+    PinkComunicateBtn: () => <button className= {'layout__btn--standar layout__btn--comunicate-pink'}>Crea un comunicado</button>,
+    BigComunicateBtn: () => <button className= {'layout__btn--standar layout__btn--comunicate-big'}>Crea tu comunicado</button>,
+    BasicPackBtn: () => <button className= {'layout__btn--standar layout__btn--package'}>Obtener Básico</button>,
+    StandarPackBtn: () => <button className= {'layout__btn--standar layout__btn--package'}>Obtener Estandar</button>,
+    PremiumPackBtn: () => <button className= {'layout__btn--standar layout__btn--package'}>Obtener Premium</button>,
 }
 
 export default Buttons;
