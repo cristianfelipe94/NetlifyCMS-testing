ace0ec1560b173a750d0f871dd8c0bf28aae2a10
unknown <cristianpipe0494@gmail.com>
2019-10-10 08:54:16 -0600

    add: buttons collection

---
+++ src/components/ComponentButtons/buttons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Buttons = {
+    SubscribeBtn: () => <button>Subscribirse</button>,
+    WhiteComunicateBtn: () => <button>Crea un comunicado</button>,
+    PinkComunicateBtn: () => <button>Crea un comunicado</button>,
+    BigComunicateBtn: () => <button>Crea tu comunicado</button>,
+    BasicPackBtn: () => <button>Obtener Básico</button>,
+    StandarPackBtn: () => <button>Obtener Estandar</button>,
+    PremiumPackBtn: () => <button>Obtener Premium</button>,
+}
+
+export default Buttons;
