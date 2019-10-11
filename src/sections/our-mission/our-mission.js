a34dcae4140bb2cf9953484f9e1cabe3c20c470f
Allison Zamora <a.zamora@WEB06.local>
2019-10-11 17:14:05 -0600

    Fix: the indentation in many files

--- src/sections/our-mission/our-mission.js
+++ src/sections/our-mission/our-mission.js
@@ -5,40 +5,40 @@ import './our-mission.scss';
 
 export default () => {
 
-    useEffect(ourMissionCanvas);
+  useEffect(ourMissionCanvas);
 
-    return (
-        <section className="our-mission">
-            <canvas className="our-mission__canvas"></canvas>
-            <Layout>
-                <h2 className="our-mission__title">Nuestra misión</h2>
-                <p className="our-mission__paragraph">
-                    Trasladar ideas valiosas de quienes las crean a
-                    quienes pueden darlas a conocer.
+  return (
+    <section className="our-mission">
+      <canvas className="our-mission__canvas"></canvas>
+      <Layout>
+        <h2 className="our-mission__title">Nuestra misión</h2>
+        <p className="our-mission__paragraph">
+          Trasladar ideas valiosas de quienes las crean a
+          quienes pueden darlas a conocer.
                 </p>
-                <div className="our-mission__svg-group svg-group--spacing">
-                    <div className="our-mission__svg light-bulb"/>
-                    <div className="our-mission__svg light-bulb"/>
-                    <div className="our-mission__svg paper-plane"/>
-                    <div className="our-mission__svg planet"/>
-                </div>
-                <h2 className="our-mission__title">Nuestra Visión</h2>
-                <p className="our-mission__paragraph">
-                    Ser la fuente confiable de distribución de
-                    contenido en el mercado centroamericano,
-                    al enlazar empresas y medios de comunicación.
+        <div className="our-mission__svg-group svg-group--spacing">
+          <div className="our-mission__svg light-bulb" />
+          <div className="our-mission__svg light-bulb" />
+          <div className="our-mission__svg paper-plane" />
+          <div className="our-mission__svg planet" />
+        </div>
+        <h2 className="our-mission__title">Nuestra Visión</h2>
+        <p className="our-mission__paragraph">
+          Ser la fuente confiable de distribución de
+          contenido en el mercado centroamericano,
+          al enlazar empresas y medios de comunicación.
                 </p>
-                <div className="our-mission__svg-group">
-                    <div className="our-mission__svg check"/>
-                    <div className="our-mission__svg check"/>
-                    <div className="our-mission__svg check"/>
-                    <div className="our-mission__svg check"/>
-                    <div className="our-mission__svg check"/>
-                    <div className="our-mission__svg check"/>
-                </div>
-                <div className="our-mission__logo"/>
-                <div className="our-mission__cta"/>
-            </Layout>
-        </section>
-    );
+        <div className="our-mission__svg-group">
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+        </div>
+        <div className="our-mission__logo" />
+        <div className="our-mission__cta" />
+      </Layout>
+    </section>
+  );
 }
