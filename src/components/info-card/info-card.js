0cd02e344296982e3ffd85d493f11e215c905ad8
Allison Zamora <a.zamora@WEB06.local>
2019-10-11 17:06:58 -0600

    enhance: I applied css  standars in how-we-work.scss

--- src/components/info-card/info-card.js
+++ src/components/info-card/info-card.js
@@ -1,20 +1,20 @@
 import React from "react";
 import "./info-card.scss";
 
-export default ({ title, paragraph, button, id, src,modifier}) => {
+export default ({ title, paragraph, button, id, src, modifier }) => {
 
-	return (
-		<div className={`container ${modifier}`}>
-			<div className="container__info">
-				<h3 className="info__tile">{title}</h3>
-				<p className="info__paragraph">{paragraph}</p>
-				<button className="info__button">Crea un comunicado{button}</button>
-			</div>
+  return (
+    <div className={`container ${modifier}`}>
+      <div className="container__info">
+        <h3 className="info__tile">{title}</h3>
+        <p className="info__paragraph">{paragraph}</p>
+        <button className="info__button">Crea un comunicado{button}</button>
+      </div>
 
-			<div className="container__animation" id={id}>
-				<img className={id} src={src} />
-			</div>
-		</div>
-	)
+      <div className="container__animation" id={id}>
+        <img className={id} src={src} />
+      </div>
+    </div>
+  )
 
 }
--- src/sections/how-we-work/how-we-work.js
+++ src/sections/how-we-work/how-we-work.js
@@ -3,29 +3,29 @@ import Layout from '../../components/layout/layout'
 import InfoCard from '../../components/info-card/info-card'
 
 export default () => {
-    return (
-        <section>
-            <Layout>
-                <InfoCard
-                    title="Creación y correción de estilos"
-                    paragraph="Revisamos los comunicados de prensa y recomendamos cambios previo al envío."
-                    id="comunicado"
-                    src = "comunicado.svg"
-                />
-                <InfoCard 
-                    modifier="container--reverse"
-                    title="Distribución automatizada "
-                    paragraph="Envíamos a medios y periodistas de acuerdo al público meta y la categoría."
-                    id="media"
-                    src = "media.svg" 
-                />
-                <InfoCard
-                    title="Seguimiento"
-                    paragraph="Hacemos un reporte de análisis de desempeño del comunicado enviado."
-                    id="grafica"
-                    src = "grafica.svg"
-                />
-            </Layout>
-        </section>
-    )
+  return (
+    <section>
+      <Layout>
+        <InfoCard
+          title="Creación y correción de estilos"
+          paragraph="Revisamos los comunicados de prensa y recomendamos cambios previo al envío."
+          id="comunicado"
+          src="comunicado.svg"
+        />
+        <InfoCard
+          modifier="container--reverse"
+          title="Distribución automatizada "
+          paragraph="Envíamos a medios y periodistas de acuerdo al público meta y la categoría."
+          id="media"
+          src="media.svg"
+        />
+        <InfoCard
+          title="Seguimiento"
+          paragraph="Hacemos un reporte de análisis de desempeño del comunicado enviado."
+          id="grafica"
+          src="grafica.svg"
+        />
+      </Layout>
+    </section>
+  )
 }
