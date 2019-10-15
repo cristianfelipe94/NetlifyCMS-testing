986abd09972b6716818fd8315757ceac6b51b5f8
unknown <cristianpipe0494@gmail.com>
2019-10-14 18:36:59 -0600

    fix: indentation to 2 spaces

--- src/sections/our-packages/our-packages.js
+++ src/sections/our-packages/our-packages.js
@@ -1,22 +1,18 @@
-import React from "react";
-import Layout from "../../components/layout/layout";
-import Package from "../../components/package/package";
+import React from "react"
+import Layout from "../../components/layout/layout"
+import Package from "../../components/package/package"
 
-import "./our-packages.scss";
+import "./our-packages.scss"
 
 export default () => {
-    return (
-        <section>
-            <Layout>
-                <h2 className= "section__title">
-                    Nuestros paquetes
-                </h2>
-                <div className= "section__bundler">
-                    <Package/>
-                </div>
-            </Layout>
-        </section>
-    );
-};
-
-
+  return (
+    <section>
+      <Layout>
+        <h2 className="section__title">Nuestros paquetes</h2>
+        <div className="section__bundler">
+          <Package />
+        </div>
+      </Layout>
+    </section>
+  )
+}
