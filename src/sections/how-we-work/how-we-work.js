94602efd23e843f27f62443abdb9120ad05ecc16
Allison Zamora <a.zamora@WEB06.local>
2019-10-14 18:08:12 -0600

    Mejora del codigo segun el pull request

--- src/sections/how-we-work/how-we-work.js
+++ src/sections/how-we-work/how-we-work.js
@@ -3,28 +3,38 @@ import Layout from '../../components/layout/layout'
 import InfoCard from '../../components/info-card/info-card'
 
 export default () => {
+
+  const cards = [
+    {
+      title: 'Creación y correción de estilos',
+      paragraph: 'Revisamos los comunicados de prensa y recomendamos cambios previo al envío.',
+      link: 'Crea un comunicado >',
+      id: 'comunicado',
+      src: 'comunicado.svg'
+    },
+    {
+      title: 'Distribución automatizada',
+      paragraph: 'Envíamos a medios y periodistas de acuerdo al público meta y la categoría.',
+      link: 'Crea un comunicado >',
+      id: 'media',
+      src: 'media.svg',
+      modifier: 'container--reverse'
+    },
+    {
+      title: 'Seguimiento',
+      paragraph: 'Hacemos un reporte de análisis de desempeño del comunicado enviado.',
+      link: 'Crea un comunicado >',
+      id: 'grafica',
+      src: 'grafica.svg'
+    }
+  ]
+
   return (
     <section>
       <Layout>
-        <InfoCard
-          title="Creación y correción de estilos"
-          paragraph="Revisamos los comunicados de prensa y recomendamos cambios previo al envío."
-          id="comunicado"
-          src="comunicado.svg"
-        />
-        <InfoCard
-          modifier="container--reverse"
-          title="Distribución automatizada "
-          paragraph="Envíamos a medios y periodistas de acuerdo al público meta y la categoría."
-          id="media"
-          src="media.svg"
-        />
-        <InfoCard
-          title="Seguimiento"
-          paragraph="Hacemos un reporte de análisis de desempeño del comunicado enviado."
-          id="grafica"
-          src="grafica.svg"
-        />
+        {
+          cards.map(card => <InfoCard {...card}/>)
+        }
       </Layout>
     </section>
   )
