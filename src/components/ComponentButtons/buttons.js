34dec68fca5cdf6589d82085481290f1a2f1c2dd
unknown <cristianpipe0494@gmail.com>
2019-10-14 18:54:52 -0600

    add: children as props and default class for Btn

--- src/components/ComponentButtons/buttons.js
+++ src/components/ComponentButtons/buttons.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './buttons.scss';
-import classNames from 'classnames';
+import React from "react"
+import PropTypes from "prop-types"
+import "./buttons.scss"
+import classNames from "classnames"
 
-function Button ({ title, button, type, action, defaultStyle, customStyle}) {
-    const mainClassNames = classNames(`${defaultStyle} btn--primary ${customStyle}`);
-    const secondaryClassNames = classNames(`${defaultStyle} btn--secondary ${customStyle}`)
-    const btnStyle = button === 'primary' ? mainClassNames : secondaryClassNames;
-    return (
-        <button className= {btnStyle} type= {type} onClick= {action} >
-            {title}
-        </button>
-    );
+function Button({ children, button, type, action, customStyle }) {
+  const mainClassNames = classNames(
+    `btn btn--primary ${customStyle}`
+  )
+  const secondaryClassNames = classNames(
+    `btn btn--secondary ${customStyle}`
+  )
+  const btnStyle = button === "primary" ? mainClassNames : secondaryClassNames
+  return (
+    <button className={btnStyle} type={type} onClick={action}>
+      {children}
+    </button>
+  )
 }
 
 Button.defaultProps = {
-    action: () => console.log("Default function"),
-    title: "Botón",
-    defaultStyle: 'btn',
-    customStyle: "",
-    button: PropTypes.oneOf(['primary', 'secondary']),
-    type: PropTypes.oneOf(['submit', 'reset', 'button']),
+  action: () => console.log("Default function"),
+  children: "Botón",
+  customStyle: "",
+  button: PropTypes.oneOf(["primary", "secondary"]),
+  type: PropTypes.oneOf(["submit", "reset", "button"]),
 }
 
 Button.propTypes = {
-    action: PropTypes.func,
-    title: PropTypes.string,
-    defaultStyle: PropTypes.string,
-    customStyle: PropTypes.string,
-    button: PropTypes.string,
-    type: PropTypes.string,
+  action: PropTypes.func,
+  children: PropTypes.node,
+  customStyle: PropTypes.string,
+  button: PropTypes.string,
+  type: PropTypes.string,
 }
 
-export default Button;
+export default Button
