7697d078c76bb2272c203ab1d0b24afa9f04b201
unknown <cristianpipe0494@gmail.com>
2019-10-16 12:25:04 -0600

    add: functionality, type, custom styles and default values to social media btns

--- src/components/component-social/social.js
+++ src/components/component-social/social.js
@@ -1,19 +1,29 @@
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import classNames from "classnames"
 import "./social.scss";
 
-const Social = ({platform, action}) => {
+const Social = ({platform, action, type, customStyle}) => {
+  const mainClassNames = classNames(
+    `social social--${platform} ${customStyle}`
+  )
   return(
-    <button className={`social social--${platform}`} onClick={action}></button>
+    <button className={mainClassNames} onClick={action} type={type}/>
   );
 };
 
 Social.defaultProps = {
   platform: "facebook",
+  action: () => {console.log("Default social media icon was clicked.")},
+  type: PropTypes.oneOf(["submit", "reset", "button"]),
+  customStyle: "",
 }
 
 Social.propTypes = {
   platform: PropTypes.string,
+  action: PropTypes.func,
+  type: PropTypes.string,
+  customStyle: PropTypes.string,
 }
 
 export default Social;
