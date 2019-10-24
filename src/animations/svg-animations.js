f9028533f0d0b8b905275126388087260774ceb6
Alizamora <39492909+Alizamora@users.noreply.github.com>
2019-10-24 11:21:30 -0600

    Arreglo el problema de las animaciones
    Cuando sé corría el comando npm run build no tenía el plugin de CSS y no podía utilizar las animaciones, solo había que hacer referencia a el mismo para que funcionara.

--- src/animations/svg-animations.js
+++ src/animations/svg-animations.js
@@ -1,4 +1,7 @@
 import { TimelineMax } from "gsap/all"
+import CSSPlugin from 'gsap/CSSPlugin';
+
+const C = CSSPlugin;  // here is the gotcha....
 
 export default function svgAnimations(animations, base) {
   Object.keys(animations).map((txt, i) => {
@@ -11,7 +14,7 @@ export default function svgAnimations(animations, base) {
       .then(res => res.text())
       .then(svg => {
         parent.innerHTML = svg + parent.innerHTML
-        //parent.style.setProperty("display", "none")
+        parent.style.setProperty("display", "none")
         if (!i) animations[txt](`#${txt}`, new TimelineMax())
         return 1
       })
