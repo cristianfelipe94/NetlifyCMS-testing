d7d2e37c137e9078b61881d5de59108284602e39
unknown <cristianpipe0494@gmail.com>
2019-10-28 11:53:40 -0600

    add: space to OurMission and Footer

--- src/sections/our-mission/our-mission.js
+++ src/sections/our-mission/our-mission.js
@@ -8,34 +8,36 @@ export default () => {
   return (
     <section className="our-mission">
       <canvas className="our-mission__canvas"></canvas>
-      <h2 className="our-mission__title">Nuestra misión</h2>
-      <p className="our-mission__paragraph">
-        Trasladar ideas valiosas a quienes puedan darlas a conocer.
-      </p>
-      <div className="our-mission__svg-group svg-group--spacing">
-        <div className="our-mission__svg light-bulb" />
-        <div className="our-mission__svg light-bulb" />
-        <div className="our-mission__svg paper-plane" />
-        <div className="our-mission__svg planet" />
-      </div>
-      <h2 className="our-mission__title">Nuestra Visión</h2>
-      <p className="our-mission__paragraph">
-        Ser la fuente confiable de distribución de contenido en el mercado
-        centroamericano, al enlazar empresas y medios de comunicación.
-      </p>
-      <div className="our-mission__svg-group">
-        <div className="our-mission__svg check" />
-        <div className="our-mission__svg check" />
-        <div className="our-mission__svg check" />
-        <div className="our-mission__svg check" />
-        <div className="our-mission__svg check" />
-        <div className="our-mission__svg check" />
-      </div>
-      <div className="our-mission__logo" />
-      <div className="cta">
-        <div className="our-mission__svg cta__paper-plane--dark" />
-        <div className="our-mission__svg cta__paper-plane" />
-        <button className="cta__btn">Crea tu comunicado</button>
+      <div className="section__wrapper">
+        <h2 className="our-mission__title">Nuestra misión</h2>
+        <p className="our-mission__paragraph">
+          Trasladar ideas valiosas a quienes puedan darlas a conocer.
+        </p>
+        <div className="our-mission__svg-group svg-group--spacing">
+          <div className="our-mission__svg light-bulb" />
+          <div className="our-mission__svg light-bulb" />
+          <div className="our-mission__svg paper-plane" />
+          <div className="our-mission__svg planet" />
+        </div>
+        <h2 className="our-mission__title">Nuestra Visión</h2>
+        <p className="our-mission__paragraph">
+          Ser la fuente confiable de distribución de contenido en el mercado
+          centroamericano, al enlazar empresas y medios de comunicación.
+        </p>
+        <div className="our-mission__svg-group">
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+          <div className="our-mission__svg check" />
+        </div>
+        <div className="our-mission__logo" />
+        <div className="cta">
+          <div className="our-mission__svg cta__paper-plane--dark" />
+          <div className="our-mission__svg cta__paper-plane" />
+          <button className="cta__btn">Crea tu comunicado</button>
+        </div>
       </div>
     </section>
   )
