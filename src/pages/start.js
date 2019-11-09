9e6fb900208d6b95a08810fd07806b286865a91f
Alisson Zamora <alison.zam18@gmail.com>
2019-11-09 01:29:29 -0600

    Add: New page

---
+++ src/pages/start.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SEO from "../components/seo";
+import Message from "../sections/message/message";
+
+const start = () => (
+  <>
+    <SEO title="Message" />
+    <Message />
+  </>
+)
+
+export default start;
