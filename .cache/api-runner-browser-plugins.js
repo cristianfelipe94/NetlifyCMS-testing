5beef1f2d30990d670dfefe92deaca95b5b4e6d1
unknown <cristianpipe0494@gmail.com>
2019-11-22 09:33:01 -0600

    Have added develop branch

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('C:/Users/Felipe/Desktop/atana-fe/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"atana","short_name":"starter","start_url":"/","background_color":"#444444","theme_color":"#444444","display":"minimal-ui","icon":"src/images/atana-logo.png"},
+    },{
+      plugin: require('C:/Users/Felipe/Desktop/atana-fe/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
