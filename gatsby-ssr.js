b20d1f46db62a10c169fcfa09b1faa587b588a61
unknown <cristianpipe0494@gmail.com>
2019-11-22 16:31:12 -0600

    Have added a server rendering to load scripts into the head and body of the html.js main file.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from "react";
 
-// You can delete this file if you're not using it
+export function onRenderBody({ setHeadComponents, setPostBodyComponents }) {
+  setHeadComponents([
+    <script
+    type="text/javascript"
+    src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+    />,
+  ]);
+  setPostBodyComponents([
+    <script
+    dangerouslySetInnerHTML={{
+      __html=`
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        });
+      }
+      `
+    }}/>,
+  ]);
+}
