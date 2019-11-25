66cf837b0494e454c7eb593ae496a617a0a39fc1
unknown <cristianpipe0494@gmail.com>
2019-11-25 16:27:27 -0600

    Have removed query as createPages is not working.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,26 @@
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
-  console.log("Function: ", createPage);
-  return graphql(`
-    {
-      query MyQuery{
-        allMarkdownRemark {
-          edges {
-            node {
-              id
-              html
-              frontmatter {
-                date
-                description
-                title
-              }
-            }
-          }
-        }
-      }
-  `).then((query => {
-    console.log("Result is: ", query)
-  })).catch(err => {
-    console.log("Error is: ", err)
-  })
-}
+// exports.createPages = ({ actions, graphql }) => {
+//   const { createPage } = actions
+//   console.log("Function: ", createPage);
+//   return graphql(`
+//     {
+//       query MyQuery{
+//         allMarkdownRemark {
+//           edges {
+//             node {
+//               id
+//               html
+//               frontmatter {
+//                 date
+//                 description
+//                 title
+//               }
+//             }
+//           }
+//         }
+//       }
+//   `).then((query => {
+//     console.log("Result is: ", query)
+//   })).catch(err => {
+//     console.log("Error is: ", err)
+//   })
+// }
