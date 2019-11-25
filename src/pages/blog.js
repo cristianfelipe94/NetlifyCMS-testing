599324aed212fd4b19f51703dc19d40327ed984e
unknown <cristianpipe0494@gmail.com>
2019-11-25 16:31:33 -0600

    Have removed query in blog page.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,44 +1,44 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+// import React from "react"
+// import { StaticQuery, graphql } from "gatsby"
 
-const Blog = () => (
-  <StaticQuery
-    query={
-      graphql`{
-        allMarkdownRemark {
-          edges {
-            node {
-              html
-              frontmatter {
-                date (formatString: "DD MMMM, YYYY")
-                description
-                title
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data) => {
-      console.log(data)
-      const parsedPosts = data.allMarkdownRemark.edges.map((post) => {
-        return (
-          <div key={`${post.node.frontmatter.title}-${post.node.frontmatter.date}`}>
-            <h2>{post.node.frontmatter.title}</h2>
-            <p>{post.node.frontmatter.description}</p>
-            <p>{post.node.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{__html: post.node.html}}/>
-          </div>
-        )
-      })
-      return (
-        <div>
-          <h2>Ultimas noticias de Átana</h2>
-          {parsedPosts}
-        </div>
-      )
-    }}
-  ></StaticQuery>
-)
+// const Blog = () => (
+//   <StaticQuery
+//     query={
+//       graphql`{
+//         allMarkdownRemark {
+//           edges {
+//             node {
+//               html
+//               frontmatter {
+//                 date (formatString: "DD MMMM, YYYY")
+//                 description
+//                 title
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={(data) => {
+//       console.log(data)
+//       const parsedPosts = data.allMarkdownRemark.edges.map((post) => {
+//         return (
+//           <div key={`${post.node.frontmatter.title}-${post.node.frontmatter.date}`}>
+//             <h2>{post.node.frontmatter.title}</h2>
+//             <p>{post.node.frontmatter.description}</p>
+//             <p>{post.node.frontmatter.date}</p>
+//             <div dangerouslySetInnerHTML={{__html: post.node.html}}/>
+//           </div>
+//         )
+//       })
+//       return (
+//         <div>
+//           <h2>Ultimas noticias de Átana</h2>
+//           {parsedPosts}
+//         </div>
+//       )
+//     }}
+//   ></StaticQuery>
+// )
 
-export default Blog
+// export default Blog
