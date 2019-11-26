d319c9132909ce9397a42a17ff1b98285f28322e
unknown <cristianpipe0494@gmail.com>
2019-11-26 11:21:40 -0600

    Have clean Gatsby cache as template key is not showing up.

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('C:/Users/Felipe/Desktop/atana-fe/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"atana","short_name":"starter","start_url":"/","background_color":"#444444","theme_color":"#444444","display":"minimal-ui","icon":"src/images/atana-logo.png"},
-    },{
-      plugin: require('C:/Users/Felipe/Desktop/atana-fe/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
