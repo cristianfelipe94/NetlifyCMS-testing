623f22d2ff782e24d0b7541fe3ebd13279949625
unknown <cristianpipe0494@gmail.com>
2019-12-09 19:46:16 -0600

    Have changed yml structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `uploads`,
-        path: `${__dirname}/static/admin/uploads`,
+        path: `${__dirname}/static/uploads`,
       },
     },
     {
